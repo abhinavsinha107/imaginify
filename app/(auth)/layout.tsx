552db398994092e2e40a356c7bce4dfbfd7a94f5
Abhinav Sinha <abhinavsinha135@gmail.com>
2024-05-06 02:16:13 +0530

    initial setup nav-menu-sidebar created

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+import { PropsWithChildren } from "react"
+
+const Layout = ({children}: PropsWithChildren) => {
+  return (
+    <main className="auth">{children}</main>
+  )
+}
+export default Layout

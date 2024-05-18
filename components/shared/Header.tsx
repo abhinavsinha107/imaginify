fa1965fa98cd1d8ad9c78a29b702597d8136a8f0
Abhinav Sinha <abhinavsinha135@gmail.com>
2024-05-19 01:25:01 +0530

    transformation form WIP...

---
+++ components/shared/Header.tsx
@@ -0,0 +1,14 @@
+type HeaderPropTypes = {
+  title: string;
+  subtitle?: string;
+};
+
+const Header = ({ title, subtitle }: HeaderPropTypes) => {
+  return (
+    <>
+      <h2 className="h2-bold text-dark-600">{title}</h2>
+      {subtitle && <p className="p-16-regular mt-4">{subtitle}</p>}
+    </>
+  );
+};
+export default Header;
